4a273e459c41449f030af779165afe60f623bc39
Christopher-Au <38948162+Christopher-Au@users.noreply.github.com>
2018-10-24 07:40:04 -0500

    Added Files

---
+++ Catalina/src/catalina/Catalina.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package catalina;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import static javafx.scene.input.KeyCode.A;
+import static javafx.scene.input.KeyCode.Z;
+
+/**
+ *
+ * @author austichr
+ */
+public class Catalina {
+        //[A-Z]
+    /**
+     * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+    Scanner scan = new Scanner(new File("pp.txt"));
+    
+    if(scan.hasNext()){
+        String temp ="";
+        String tempTwo="";
+        for (int x=0; x<temp.length(); x++){
+            if temp.charAt(x).equals([A-Z]){
+                
+            }
+        }
+        
+        
+        }
+        }
+    }
+    
+    
+
+
